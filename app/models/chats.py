79d9734f3f1d3e1e060d7f7ef1157bb0a86f141c
apusetone <69253237+apusetone@users.noreply.github.com>
2024-05-09 20:15:57 +0900

    use cte on chat get_all

--- app/models/chats.py
+++ app/models/chats.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from typing import AsyncIterator
 
-from sqlalchemy import Column, ForeignKey, Index, Integer, String, select
+from sqlalchemy import Column, ForeignKey, Index, Integer, String, or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, relationship
 
 from app.commons.types import ChatType
 
 from .base import TimestampedEntity
+from .chat_participants import ChatParticipants
 from .users import User
 
 
@@ -37,9 +38,22 @@ class Chat(TimestampedEntity):
     async def read_all(
         cls, session: AsyncSession, user_id: int, offset: int, limit: int, desc: bool
     ) -> AsyncIterator[Chat]:
+        # CTE (Common Table Expression) を定義
+        user_chats_cte = (
+            select(ChatParticipants.chat_id)
+            .where(ChatParticipants.user_id == user_id)
+            .cte("user_chats")
+        )
+
+        # メインクエリでCTEを使用
         stmt = (
             select(cls)
-            .where(cls.created_by == user_id)
+            .where(
+                or_(
+                    cls.id.in_(select(user_chats_cte.c.chat_id)),
+                    cls.created_by == user_id,
+                )
+            )
             .order_by(cls.id.desc() if desc else cls.id)
             .offset(offset)
             .limit(limit)
